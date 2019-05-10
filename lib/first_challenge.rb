98a4dabe6debefb0824c54d09092752069a81b4e
Your Name <you@example.com>
2019-05-10 12:08:29 -0400

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -13,10 +13,20 @@ def first_challenge
     }
   }
 
-  #your code here
+  #your code herecontacts.each do |person, data|
+  contacts.each do |person, data|#for each entry in the big hash jon snow/ freddy mercury(person-key) and (value-data)
 
+       data.each do |key, value|#for each entry in the inner hash name,email,ice_cream,knows
+          if key == :favorite_icecream_flavors#if there is a key with this value do ...
+            #using enumerable to iterate through the ice cream array asking the question..
+            #for each ice_cream value delete it if the flavor IS strawberries
+              value.delete_if do |flavor|
+              flavor == "strawberry"
+          end
+      end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
+end
